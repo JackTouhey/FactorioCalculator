7a34aa3f3c9fa8887f9b3014f88f426616f43fff
Jack Touhey <jt112112@gmail.com>
2025-01-06 11:39:09 +1300

    Initial Commit

---
+++ FactorioCalculatorView/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FactorioCalculatorView;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FactorioCalculatorView/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FactorioCalculatorView;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
